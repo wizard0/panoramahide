0eea2f97b56ddad2654c1e2c245fa4df7df6ed69
Wizard <w.izard@outlook.com>
2018-11-20 16:13:16 +0300

    Relation changed on many2many between journal and publishing and between journal and category tables.

--- database/migrations/2018_11_20_072012_add_foreign_keys_to_tables.php
+++ database/migrations/2018_11_20_072012_add_foreign_keys_to_tables.php
@@ -14,9 +14,27 @@ class AddForeignKeysToTables extends Migration
     public function up()
     {
         Schema::table('journal', function (Blueprint $table) {
-            $table->foreign('publishing_id')->references('id')->on('publishing');
-            $table->foreign('category_id')->references('id')->on('category');
             $table->foreign('journal_contact_id')->references('id')->on('journal_contact');
+        });
+
+        Schema::create('journals_publishings', function (Blueprint $table) {
+            $table->unsignedInteger('journal_id');
+            $table->unsignedInteger('publishing_id');
+
+            $table->foreign('journal_id')->references('id')->on('journal');
+            $table->foreign('publishing_id')->references('id')->on('publishing');
+
+            $table->primary(['journal_id', 'publishing_id']);
+        });
+
+        Schema::create('journals_categories', function (Blueprint $table) {
+            $table->unsignedInteger('journal_id');
+            $table->unsignedInteger('category_id');
+
+            $table->foreign('journal_id')->references('id')->on('journal');
+            $table->foreign('category_id')->references('id')->on('category');
+
+            $table->primary(['journal_id', 'category_id']);
         });
 
         Schema::table('journal_translate', function (Blueprint $table) {
