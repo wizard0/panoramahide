822ea6747b413830f21e0f3924935d6f8a1c4870
NikolayGrishchenko <grishchenkonikolay@gmail.com>
2019-02-05 03:24:54 -0500

    Issue#33 :: deleted some voyager admin resources and implemented base template of new admin dashboard

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,27 +10,76 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.js') }}"></script>
 
-    <link rel="shortcut icon" href="{{{ asset('img/favicon.png') }}}">
+    <link rel="shortcut icon" href="{{ asset('img/favicon.png') }}">
 
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+    <!-- Fontfaces CSS-->
+    <link href="{{ asset('css/admin/font-face.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/font-awesome-4.7/css/font-awesome.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/font-awesome-5/css/fontawesome-all.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/mdi-font/css/material-design-iconic-font.min.css') }}" rel="stylesheet" media="all">
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <!-- Bootstrap CSS-->
+    <link href="{{ asset('vendor/bootstrap-4.1/bootstrap.min.css') }}" rel="stylesheet" media="all">
+
+    <!-- Vendor CSS-->
+    <link href="{{ asset('vendor/animsition/animsition.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/wow/animate.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/css-hamburgers/hamburgers.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/slick/slick.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/select2/select2.min.css') }}" rel="stylesheet" media="all">
+    <link href="{{ asset('vendor/perfect-scrollbar/perfect-scrollbar.css') }}" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="{{ asset('css/admin/theme.css') }}" rel="stylesheet" media="all">
+
+
 </head>
-<body>
-@include('includes.admin.header')
+<body class="animsition">
+    <div class="page-wrapper">
+        @include('includes.admin.header_mobile')
+        @include('includes.admin.menu_sidebar')
 
-@yield('content')
+        <!-- PAGE CONTAINER-->
+        <div class="page-container">
+        @include('includes.admin.header_desktop')
 
-@include('includes.admin.footer')
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+
+                        @yield('content')
+
+                        @include('includes.admin.footer')
+                    </div>
+                </div>
+            </div>
+                <!-- END MAIN CONTENT-->
+                <!-- END PAGE CONTAINER-->
+        </div>
+    </div>
+
+    <!-- Jquery JS-->
+    <script src="{{ asset('/vendor/jquery-3.2.1.min.js') }}"></script>
+    <!-- Bootstrap JS-->
+    <script src="{{ asset('/vendor/bootstrap-4.1/popper.min.js') }}"></script>
+    <script src="{{ asset('/vendor/bootstrap-4.1/bootstrap.min.js') }}"></script>
+    <!-- Vendor JS       -->
+    <script src="{{ asset('/vendor/slick/slick.min.js') }}"></script>
+    <script src="{{ asset('/vendor/wow/wow.min.js') }}"></script>
+    <script src="{{ asset('/vendor/animsition/animsition.min.js') }}"></script>
+    <script src="{{ asset('/vendor/bootstrap-progressbar/bootstrap-progressbar.min.js') }}"></script>
+    <script src="{{ asset('/vendor/counter-up/jquery.waypoints.min.js') }}"></script>
+    <script src="{{ asset('/vendor/counter-up/jquery.counterup.min.js') }}"></script>
+    <script src="{{ asset('/vendor/circle-progress/circle-progress.min.js') }}"></script>
+    <script src="{{ asset('/vendor/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('/vendor/chartjs/Chart.bundle.min.js') }}"></script>
+    <script src="{{ asset('/vendor/select2/select2.min.js') }}"></script>
+
+    <!-- Main JS-->
+    <script src="{{ asset('js/admin/main.js') }}"></script>
 
 </body>
 </html>
