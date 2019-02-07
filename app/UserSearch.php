3e33984f13cbf5e2c07fd34fc63ab4c15a212f20
Денис Парыгин <dyp2000@mail.ru>
2019-02-07 16:50:19 +0500

    Добавлены ковыки для строковых значений

--- app/UserSearch.php
+++ app/UserSearch.php
@@ -174,11 +174,11 @@ class UserSearch extends Model
             }
 
             // Translations
-            $q = $q->where('journal_translations.locale' , '=', $searchLocale)
-                ->where('release_translations.locale' , '=', $searchLocale)
-                ->where('article_translations.locale' , '=', $searchLocale)
-                ->where('author_translations.locale' , '=', $searchLocale)
-                ->where('category_translations.locale' , '=', $searchLocale);
+            $q = $q->where('journal_translations.locale', '=', "'".$searchLocale."'")
+                ->where('release_translations.locale', '=', "'".$searchLocale."'")
+                ->where('article_translations.locale', '=', "'".$searchLocale."'")
+                ->where('author_translations.locale', '=', "'".$searchLocale."'")
+                ->where('category_translations.locale', '=', "'".$searchLocale."'");
 
             // Activity
             $q = $q->where('journals.active', '=', '1')
