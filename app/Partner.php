47705bbe8b75e606854dacb782db449a3d0b57b5
Ilya Kartunin <ikartunin@gmail.com>
2019-02-12 11:42:18 +0300

    issue #23 :: Модель партнёра

--- app/Partner.php
+++ app/Partner.php
@@ -2,9 +2,23 @@
 
 namespace App;
 
+use App\Traits\ActiveField;
 use Illuminate\Database\Eloquent\Model;
 
 class Partner extends Model
 {
-    //
+    use ActiveField;
+
+    protected $fillable = [
+        'secret_key', 'active'
+    ];
+
+    public function users()
+    {
+        return $this->hasMany(PartnerUser::class);
+    }
+    public function quotas()
+    {
+        return $this->hasMany(Quota::class);
+    }
 }
