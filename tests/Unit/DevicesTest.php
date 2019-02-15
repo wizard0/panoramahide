17670a65e326be29ed53fed9fa99b4778d09fb37
Ilya Kartunin <ikartunin@gmail.com>
2019-02-15 11:39:13 +0300

    issue #26 :: Тесты по устройствам

--- tests/Unit/DevicesTest.php
+++ tests/Unit/DevicesTest.php
@@ -4,105 +4,84 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use App\Models\Device;
+use App\Models\Partner;
 use App\Models\PartnerUser;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+//use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 use Carbon\Carbon;
 
 class DevicesTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
+    use DatabaseTransactions;
+
     //Тесты по девайсам юзеров партнёров
-    public function testPartnerUserCreateDevice()
+    public function testPartnerUserDevice()
     {
-        $result = true;
-        for ($i=0; $i < 10; $i++) {
-            $user = PartnerUser::inRandomOrder()->first();
-            $user->createDevice();
-            if ($user->devices()->count() == 0)
-                $result = false;
-        }
-        $this->assertTrue($result);
+        $user = factory(PartnerUser::class)->create();
+        $user->createDevice();
+        $user->createDevice();
+        $user->createDevice();
+        // Проверяем, что все устройства создались
+        $this->assertTrue($user->devices()->count() == 3);
+
+        // проверяем связь устройства с юзером
+        $device = $user->devices()->inRandomOrder()->first();
+        $this->assertNotNull($device->user);
+
+        // Проверка отправки кода активации
+        $device->sendCodeToUser();
+        $this->assertNotNull($device->activate_code);
+
+        // Проверяем отправку юзеру без email
+        $user->email = null;
+        $user->save();
+        $device = $user->devices()->whereActive(false)->first();
+        $this->assertTrue(!$device->sendCodeToUser());
+
+        // Проверка, не просрочено ли не активное устройство
+        $this->assertTrue(!$device->checkActivation());
+
+        // Проверка активации по коду
+        $device = Device::activateByCode($device->activate_code);
+        $this->assertTrue($device->isActive());
+
+        // Проверка, не просрочено ли активное устройство
+        $this->assertTrue($device->checkActivation());
+
+        // Проверяем активацию устройства с неверным кодом
+        $this->assertNull(Device::activateByCode(md5(rand(0,9999))));
+
+        // Проверяем методы online
+        $device->setOnline();
+        $this->assertTrue($device->isOnline());
+
+        // Проверка сброса активных устройств
+        $user->resetAllDevices();
+        $this->assertTrue($user->devices()->whereActive(true)->count() == 0);
+
+        // Проверка просроченного устройства
+        $device = $user->devices()->first();
+        $device->activateDevice();
+        $device->activate_date = Carbon::now()->subDays(Device::ACTIVE_DAYS*2);
+        $this->assertTrue(!$device->checkActivation());
     }
-    public function testGetDeviceUser()
+
+    //Тесты по девайсам юзеров партала
+    public function testUserDevice()
     {
-        $result = true;
-        for ($i=0; $i < 10; $i++) {
-            $device = Device::inRandomOrder()->first();
-            if ($device->user == null)
-                $result = false;
-        }
-        $this->assertTrue($result);
-    }
-    public function testGetUserDevices()
-    {
-        $result = true;
-        for ($i=0; $i < 10; $i++) {
-            $user = PartnerUser::inRandomOrder()->first();
-            if ($user->devices == null)
-                $result = false;
-        }
-        $this->assertTrue($result);
-    }
-    public function testDeviceActivateByCode()
-    {
-        $result = true;
-        for ($i=0; $i < 10; $i++) {
-            $device = Device::inRandomOrder()->first();
-            $device->sendCodeToUser();
-            if ($device->activate_code == null)
-                $result = false;
-            $device = Device::whereNotNull('activate_code')->first();
-            $device = Device::activateByCode($device->activate_code);
-            if ($device->isActive() == false)
-                $result = false;
-        }
-        $this->assertTrue($result);
-    }
-    public function testResetUserDevices()
-    {
-        for ($i=0; $i < 5; $i++) {
-            $user = null;
-            do {
-                $user = PartnerUser::inRandomOrder()->first();
-            } while ($user->devices()->count() == 0);
-            if ($user->devices()->count() > $user->devices()->whereActive(true)->count()) {
-                foreach ($user->devices()->get() as $device)
-                    $device->activateDevice();
-                if ($user->devices()->count() != $user->devices()->whereActive(true)->count())
-                    return $this->assertTrue(false);
-            }
-            $user->resetAllDevices();
-            if ($user->devices()->whereActive(true)->count() != 0)
-                return $this->assertTrue(false);
-        }
-        return $this->assertTrue(true);
-    }
-    public function testActivateDevices()
-    {
-        $activeCount = Device::whereActive(true)->count();
-        foreach (Device::whereActive(false)->get() as $device) {
-            if (rand(0,1)) {
-                $activeCount++;
-                $device->activateDevice();
-            }
-        }
-        $this->assertTrue($activeCount == Device::whereActive(true)->count());
-    }
-    public function testOnlineDevices()
-    {
-        foreach (Device::whereActive(true)->get() as $device) {
-            if (rand(0,1) && !$device->isOnline()) {
-                $device->setOnline();
-                if (!$device->isOnline())
-                    return $this->assertTrue(false);
-            }
-        }
-        $this->assertTrue(true);
+        $user = factory(\App\User::class)->create();
+        $user->createDevice();
+        $user->createDevice();
+        $user->createDevice();
+        // Проверяем, что все устройства создались
+        $this->assertTrue($user->devices()->count() == 3);
+
+        // проверяем связь устройства с юзером
+        $device = $user->devices()->first();
+        $this->assertNotNull($device->user);
+
     }
 }
