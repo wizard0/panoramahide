c88c285a1ce3ca1740d3ddcab6ccdb30bb8a74a4
Ilya Kartunin <ikartunin@gmail.com>
2019-02-17 11:24:20 +0300

    issue #26 :: Тесты по устройствам

--- tests/Unit/DevicesTest.php
+++ tests/Unit/DevicesTest.php
@@ -1,4 +1,8 @@
 <?php
+/*
+ * Copyright (c) 2018-2019 "ИД Панорама"
+ * Автор модуля: Илья Картунин (ikartunin@gmail.com)
+ */
 
 namespace Tests\Unit;
 
@@ -16,90 +20,104 @@ class DevicesTest extends TestCase
 {
     use DatabaseTransactions;
 
-    //Тесты по девайсам юзеров партнёров
-    public function testPartnerUserDevice()
+    protected $user;
+    protected $device;
+
+    protected function setUp()
     {
-        self::getUserAndDevice($user, $device);
-
-        // Проверяем, что все устройства создались
-        $this->assertEquals($user->devices()->count(), 3, $this->textRed('Ошибка createDevice'));
-
-        // проверяем связь устройства с юзером
-        $device = $user->devices()->inRandomOrder()->first();
-        $this->assertNotNull($device->user, $this->textRed('Ошибка связи device->user'));
+        parent::setUp();
+        $this->getUserAndDevice();
     }
-    public function testActivationCodeDevice()
+
+    protected function tearDown()
     {
-        self::getUserAndDevice($user, $device);
-
-        // Проверка отправки кода активации
-        Mail::fake();
-        $device->sendCodeToUser();
-        $this->assertNotNull($device->activate_code, $this->textRed('Ошибка при отправке кода для активации устройства'));
-
-        // Проверяем отправку юзеру без email
-        $user->email = null;
-        $user->save();
-        $device = $user->devices()->whereActive(false)->first();
-        $this->assertTrue(!$device->sendCodeToUser(), $this->textRed('Ошибка при отправке кода пользователю без email'));
-
-        // Проверка активации по коду
-        $device = Device::activateByCode($device->activate_code);
-        $this->assertTrue($device->isActive(), $this->textRed('Ошибка при активации устройства по коду'));
-
-        // Проверяем активацию устройства с неверным кодом
-        $this->assertNull(Device::activateByCode(md5(rand(0,9999))), $this->textRed('Ошибка при проверке активации устройства с неверным кодом'));
+        unset($this->user);
+        unset($this->device);
     }
-    public function testCheckActivationDevice()
+
+    protected function getUserAndDevice($partner_user = true)
     {
-        self::getUserAndDevice($user, $device);
-
-        // Проверка, не просрочено ли не активное устройство
-        $this->assertTrue(!$device->checkActivation(), $this->textRed('Ошибка при проверке активации неактивного устройства'));
-
-        $device->activateDevice();
-
-        // Проверка, не просрочено ли активное устройство
-        $this->assertTrue($device->checkActivation(), $this->textRed('Ошибка при проверке активации активного устройства'));
-
-        // Проверка сброса активных устройств
-        $user->resetAllDevices();
-        $this->assertEquals($user->devices()->whereActive(true)->count(), 0, $this->textRed('Ошибка при проверке сброса активных устройств'));
-
-        // Проверка просроченного устройства
-        $device = $user->devices()->first();
-        $device->activateDevice();
-        $device->activate_date = Carbon::now()->subDays(Device::ACTIVE_DAYS*2);
-        $this->assertTrue(!$device->checkActivation(), $this->textRed('Ошибка при проверке активации просроченного устройства'));
-    }
-    public function testCheckOnlineDevice()
-    {
-        self::getUserAndDevice($user, $device);
-
-        // Проверяем методы online
-        $device->setOnline();
-        $this->assertTrue($device->isOnline(), $this->textRed('Ошибка при проверке методов online'));
+        $this->user = ($partner_user ? factory(PartnerUser::class)->create() : factory(\App\User::class)->create());
+        $this->user->createDevice();
+        $this->user->createDevice();
+        $this->user->createDevice();
+        $this->device = $this->user->devices()->inRandomOrder()->first();
     }
 
     //Тесты по девайсам юзеров партала
     public function testUserDevice()
     {
-        self::getUserAndDevice($user, $device, false);
-
+        $this->getUserAndDevice(false);
         // Проверяем, что все устройства создались
-        $this->assertEquals($user->devices()->count(), 3);
-
+        $this->assertEquals($this->user->devices()->count(), 3);
         // проверяем связь устройства с юзером
-        $this->assertNotNull($device->user);
+        $this->assertNotNull($this->device->user);
     }
 
-    // Данные для тестов
-    public static function getUserAndDevice(&$user, &$device, $partner_user = true)
+    //Тесты по девайсам юзеров партнёров
+    public function testPartnerUserDevice()
     {
-        $user = ($partner_user ? factory(PartnerUser::class)->create() : factory(\App\User::class)->create());
-        $user->createDevice();
-        $user->createDevice();
-        $user->createDevice();
-        $device = $user->devices()->inRandomOrder()->first();
+        // Проверяем, что все устройства создались
+        $this->assertEquals($this->user->devices()->count(), 3, $this->textRed('Ошибка createDevice'));
+        // проверяем связь устройства с юзером
+        $device = $this->user->devices()->inRandomOrder()->first();
+        $this->assertNotNull($device->user, $this->textRed('Ошибка связи device->user'));
+    }
+
+    public function testSendCodeToUser()
+    {
+        // Проверка отправки кода активации
+        Mail::fake();
+        $this->device->sendCodeToUser();
+        $this->assertNotNull($this->device->activate_code, $this->textRed('Ошибка при отправке кода для активации устройства'));
+
+        // Проверяем отправку юзеру без email
+        $this->user->email = null;
+        $this->user->save();
+        $device = $this->user->devices()->whereActive(false)->first();
+        $this->assertTrue(!$device->sendCodeToUser(), $this->textRed('Ошибка при отправке кода пользователю без email'));
+    }
+
+    public function testDeviceActivationByCode()
+    {
+        $this->device->getCode();
+
+        // Проверяем активацию устройства с неверным кодом
+        $this->assertNull(Device::activateByCode(md5(rand(0,9999))), $this->textRed('Ошибка при проверке активации устройства с неверным кодом'));
+
+        // Проверка активации по коду
+        $device = Device::activateByCode($this->device->activate_code);
+        $this->assertTrue($device->isActive(), $this->textRed('Ошибка при активации устройства по коду'));
+    }
+
+    public function testCheckActivationDevice()
+    {
+        // Проверка, не просрочено ли не активное устройство
+        $this->assertTrue(!$this->device->checkActivation(), $this->textRed('Ошибка при проверке активации неактивного устройства'));
+
+        // Проверка, не просрочено ли активное устройство
+        $this->device->activateDevice();
+        $this->assertTrue($this->device->checkActivation(), $this->textRed('Ошибка при проверке активации активного устройства'));
+
+        // Проверка просроченного устройства
+        $device = $this->user->devices()->first();
+        $device->activateDevice();
+        $device->activate_date = Carbon::now()->subDays(Device::ACTIVE_DAYS*2);
+        $this->assertTrue(!$device->checkActivation(), $this->textRed('Ошибка при проверке активации просроченного устройства'));
+    }
+    public function testResetAllDevices()
+    {
+        foreach ($this->user->devices as $device) {
+            $device->activateDevice();
+        }
+        // Проверка сброса активных устройств
+        $this->user->resetAllDevices();
+        $this->assertEquals($this->user->devices()->whereActive(true)->count(), 0, $this->textRed('Ошибка при проверке сброса активных устройств'));
+    }
+    public function testCheckOnlineDevice()
+    {
+        // Проверяем методы online
+        $this->device->setOnline();
+        $this->assertTrue($this->device->isOnline(), $this->textRed('Ошибка при проверке методов online'));
     }
 }
