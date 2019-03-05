6cdcf0110f694350ded17582ec469789caa447d1
Денис Парыгин <dyp2000@mail.ru>
2019-03-05 11:48:30 +0500

    issue #33 :: Исправление кода CRUDController::isTranslatable()

--- app/Http/Controllers/Admin/CRUDController.php
+++ app/Http/Controllers/Admin/CRUDController.php
@@ -96,14 +96,16 @@ class CRUDController extends Controller
 
     private function isTranslatable($modelName = null)
     {
+        $result = false;
         if (!$modelName && isset($this->modelName)) {
             $modelName = $this->modelName;
         }
 
         $_modelName = $modelName ? $modelName : $this->modelName;
         if (!is_null($_modelName) && class_exists($_modelName)) {
-            return in_array('Dimsav\Translatable\Translatable', class_uses($_modelName));
+            $result = in_array('Dimsav\Translatable\Translatable', class_uses($_modelName));
         }
+        return $result;
     }
 
     protected function getModel($id)
