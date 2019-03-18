5bc8917de01f38de758b5910c09292f6c4398cea
Денис Парыгин <dyp2000@mail.ru>
2019-03-18 11:48:48 +0500

    Отформатирован код шаболна

--- resources/views/admin/content/index.blade.php
+++ resources/views/admin/content/index.blade.php
@@ -2,19 +2,20 @@
 
 @section('content')
 
-   	<div style="margin-top: -30px; margin-bottom: 10px;">
-   	@php
-   		$path = explode('/', Request::path());
-   	@endphp
-	@foreach ($path as $item)
-		{{ __('admin.' . $item) }}
-		@if (end($path) != $item)
-			&nbsp;&nbsp;>&nbsp;&nbsp;
-		@endif
-    @endforeach
-   	</div>
+    <div style="margin-top: -30px; margin-bottom: 10px;">
+    @php
+        $path = explode('/', Request::path());
+    @endphp
 
-    <a class="btn btn-primary" style="margin-bottom: 20px" href="{{ route($slug . ".create") }}">{{ __('Create a Record') }}</a>
-    @component('components.admin.view_table', $data) @endcomponent
+    @foreach ($path as $item)
+        {{ __('admin.' . $item) }}
+        @if (end($path) != $item)
+            &nbsp;&nbsp;>&nbsp;&nbsp;
+        @endif
+    @endforeach
+    </div>
+
+  <a class="btn btn-primary" style="margin-bottom: 20px" href="{{ route($slug . ".create") }}">{{ __('Create a Record') }}</a>
+  @component('components.admin.view_table', $data) @endcomponent
 
 @endsection
